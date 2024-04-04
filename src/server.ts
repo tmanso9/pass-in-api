5091fd53b2375d1fc1cb043d7f3b5bbb5b2e1541
tmanso9 <tmanso92@gmail.com>
2024-04-04 19:00:45 +0100

    implement post /register for event

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/createEvent'
+import { registerForEvent } from './routes/registerForEvent'
 
 const app = Fastify()
 
@@ -8,6 +9,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
+app.register(registerForEvent)
 
 app.listen({ port: 3000 }).then(() => {
 	console.log('HTTP server running on port 3000!')
