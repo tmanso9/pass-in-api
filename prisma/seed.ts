422b87012abe9ad4ddadffa4f443052fe87aca80
tmanso9 <tmanso92@gmail.com>
2024-04-05 13:53:15 +0100

    squashed migrations and updated seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,48 +1,48 @@
 import { prisma } from '../src/lib/prisma'
 
 const seed = async () => {
-	await prisma.event.delete({
+	const alreadyExists = await prisma.event.findUnique({
 		where: {
 			id: 'cfb2f402-32a1-4388-acd2-9db8938d7110'
 		}
 	})
-
-	await prisma.event.create({
-		data: {
-			id: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
-			title: 'Unite Summit',
-			slug: 'unite-summit',
-			details: 'A conference for developers',
-			maximumAttendees: 120
-		}
-	})
-
-	await prisma.attendee.createMany({
-		data: [
-			{
-				eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
-				name: 'Alice',
-				email: 'a@b.com',
-				id: 1
-			},
-			{
-				eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
-				name: 'Benny',
-				email: 'b@b.com',
-				id: 2
-			},
-			{
-				eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
-				name: 'Charlie',
-				email: 'c@b.com'
-			},
-			{
-				eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
-				name: 'David',
-				email: 'd@b.com'
+	if (!alreadyExists) {
+		await prisma.event.create({
+			data: {
+				id: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
+				title: 'Unite Summit',
+				slug: 'unite-summit',
+				details: 'A conference for developers',
+				maximumAttendees: 120
 			}
-		]
-	})
+		})
+		await prisma.attendee.createMany({
+			data: [
+				{
+					eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
+					name: 'Alice',
+					email: 'a@b.com',
+					id: 1
+				},
+				{
+					eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
+					name: 'Benny',
+					email: 'b@b.com',
+					id: 2
+				},
+				{
+					eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
+					name: 'Charlie',
+					email: 'c@b.com'
+				},
+				{
+					eventId: 'cfb2f402-32a1-4388-acd2-9db8938d7110',
+					name: 'David',
+					email: 'd@b.com'
+				}
+			]
+		})
+	}
 }
 
 seed().then(() => {
