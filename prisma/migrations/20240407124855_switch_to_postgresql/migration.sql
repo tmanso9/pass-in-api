ba7fb2036b06069386e745380aba55c387c40492
tmanso9 <tmanso92@gmail.com>
2024-04-07 14:37:17 +0100

    feat: change to postgresql

---
+++ prisma/migrations/20240407124855_switch_to_postgresql/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "attendees" ALTER COLUMN "created_at" SET DATA TYPE TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "check_ins" ALTER COLUMN "created_at" SET DATA TYPE TIMESTAMP(3);
