4745a943c3f8a477f8874d0302df0e095b37960a
tmanso9 <tmanso92@gmail.com>
2024-04-07 14:35:52 +0100

    feat: port as an env variable

--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,7 @@ app.register(checkIn)
 
 app.setErrorHandler(errorHandler)
 
-app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
-	console.log('HTTP server running on port 3000!')
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
+app.listen({ port, host: '0.0.0.0' }).then(() => {
+	console.log(`HTTP server running! http://localhost:${port}/docs`)
 })
