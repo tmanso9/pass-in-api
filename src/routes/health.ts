7d24c19d0d2fb0dbccd61f8fd16040df38087afe
tmanso9 <tmanso92@gmail.com>
2024-04-08 12:05:21 +0100

    fix: health sends message

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -3,6 +3,6 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 
 export const health = async (app: FastifyInstance) => {
 	app.withTypeProvider<ZodTypeProvider>().get('/health', async (request, reply) => {
-		return reply.status(200).send()
+		return reply.status(200).send({ message: 'ok'})
 	})
 }
