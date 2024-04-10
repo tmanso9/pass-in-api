49870efa8c2d6413b289a33a9dbe43f2ddff89a6
tmanso9 <tmanso92@gmail.com>
2024-04-10 19:46:33 +0100

    chore: improve swagger documentation

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,8 +1,24 @@
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export const health = async (app: FastifyInstance) => {
-	app.withTypeProvider<ZodTypeProvider>().get('/health', async (request, reply) => {
-		return reply.status(200).send({ message: 'ok'})
-	})
+	app.withTypeProvider<ZodTypeProvider>().get(
+		'/health',
+		{
+			schema: {
+				response: {
+					200: z.object({
+						message: z.string()
+					})
+				},
+				tags: ['usage'],
+				summary: 'Health check',
+				description: 'Check if the API is up and running.'
+			}
+		},
+		async (request, reply) => {
+			return reply.status(200).send({ message: 'ok' })
+		}
+	)
 }
--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,12 +1,27 @@
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export const homeRoute = async (app: FastifyInstance) => {
-	app.withTypeProvider<ZodTypeProvider>().get('/', async (request, reply) => {
-		reply.status(200)
-		return reply.html`
+	app.withTypeProvider<ZodTypeProvider>().get(
+		'/',
+		{
+			schema: {
+				response: {
+					200: z.string({ description: 'Welcome page HTML code' })
+				},
+				tags: ['usage'],
+				summary: 'API Home page',
+				description: 'Welcome message for the API.',
+				produces: ['text/html']
+			}
+		},
+		async (request, reply) => {
+			reply.status(200)
+			return reply.html`
 		<h3>Welcome to pass.in API</h3>
 		<p><a href="/docs">Click here</a> for Swagger documentation.</p>
 		`
-	})
+		}
+	)
 }
